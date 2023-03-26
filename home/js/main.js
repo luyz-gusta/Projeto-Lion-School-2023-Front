582e5fc5075573ec6b72ffe9dfe8038d302e665d
LuizGustavo <luizgustavo.sp2020@gmail.com>
2023-03-26 13:45:24 -0300

    Home finalizado

--- home/js/main.js
+++ home/js/main.js
@@ -1,5 +1,37 @@
 'use strict'
 
+import {cursos} from "../../json/cursos.js"
+
+const dadosCursos = cursos
+
+export const getCursos = (cursos, indice) => {
+    const cardCurso = document.createElement('div')
+    cardCurso.classList.add('card__curso')
+
+    const imgCurso = document.createElement('img')
+    imgCurso.src = `./${dadosCursos[indice].icone}`
+    imgCurso.classList.add('img__curso')
+    
+    const nomeCurso = document.createElement('div')
+    nomeCurso.classList.add('nome__curso')
+    nomeCurso.textContent = dadosCursos[indice].sigla
+
+    cardCurso.append(imgCurso, nomeCurso)
+
+    const clickCuros = cardCurso.addEventListener('click',  (event) => {
+        window.open('http://127.0.0.1:5500/curso/index.html')
+        window.close('http://127.0.0.1:5500/home/index.html')
+    })
+
+    return cardCurso
+}
+
+const carregarCursos = () =>{
+    const containerCursos = document.querySelector('.container__cursos')
+    const listaCursos = dadosCursos.map(getCursos)
+    containerCursos.replaceChildren(...listaCursos)
+}
+
 const exit = () => {
     const buttonSair = document.querySelector('.button__sair')
     buttonSair.onclick = function () {
@@ -7,5 +39,6 @@ const exit = () => {
         console.log('teste')
     }
 }
+carregarCursos()
 
 exit()
--- json/cursos.js
+++ json/cursos.js
@@ -1,14 +1,16 @@
-var cursos = [
+export var cursos = [
     {
         "nome"  :   "001 - Técnico em Desenvolvimento de Sistemas",
         "sigla" :   "DS",
-        "icone" :   "https://image.shutterstock.com/image-vector/api-interface-vector-icon-600w-659203513.jpg",
+        "icone" :   "img/code.png",
         "carga" :   "1200",
     },
     {
         "nome"  :   "002 - Técnico em Redes de Computadores",
         "sigla" :   "RDS",
-        "icone" :   "https://img.icons8.com/ultraviolet/344/thin-client.png",
+        "icone" :   "img/redes.png",
         "carga" :   "1200"
     }
 ];
+
+
